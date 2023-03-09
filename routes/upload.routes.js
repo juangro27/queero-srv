22af8909eb1e38b0146071705b6f930a286c53d9
Catalina Fernandez <catalinaf97@gmail.com>
2023-03-09 10:27:47 +0100

    changes

--- routes/upload.routes.js
+++ routes/upload.routes.js
@@ -5,7 +5,7 @@ const uploader = require('../middlewares/uploader.middleware')
 router.post('/image', uploader.single('imageUrl'), (req, res, next) => {
 
     if (!req.file) {
-        return res.json('no image')
+        return res.sendStatus(200)
     } else {
         res.json({ cloudinary_url: req.file.path })
     }
